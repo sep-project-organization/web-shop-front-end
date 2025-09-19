9c477d54ede9041bff249757d665d623862c1df4
anciii13 <anaa.radovanovic2001@gmail.com>
2025-09-19 14:19:57 +0200

    feat: change addresses

--- web-shop-front-end/src/app/env/environment.ts
+++ web-shop-front-end/src/app/env/environment.ts
@@ -1,6 +1,7 @@
 export const environment = {
     production: false,
     //apiHost: 'http://localhost:8081/api/',
-    apiHost: 'https://localhost:8081/api/',
-    websocketUrl: 'https://localhost:8081/ws'
+    apiHost: 'https://172.20.10.5:8081/api/',
+    websocketUrl: 'https://172.20.10.5:8081/ws',
+    keycloakHost: 'http://172.20.10.5:9099'
 }
--- web-shop-front-end/src/keycloak-init.ts
+++ web-shop-front-end/src/keycloak-init.ts
@@ -1,7 +1,8 @@
 import Keycloak from 'keycloak-js';
+import { environment } from './app/env/environment';
 
 const keycloak = new Keycloak({
-  url: 'http://localhost:9099',
+  url: environment.keycloakHost,
   realm: 'sep-realm',
   clientId: 'web-shop-frontend',
 });
